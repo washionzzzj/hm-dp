51ecc010abd5cba4363513180475f032521ac541
zhongjin <zhongjin@hupu.com>
2022-09-13 17:16:18 +0800

    second version

--- src/main/java/com/hmdp/Test.java
+++ src/main/java/com/hmdp/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("123");
         for (int i = 0; i < 100; i++) {
-            System.out.println(123);
+            System.out.println(12345678);
         }
     }
 }
